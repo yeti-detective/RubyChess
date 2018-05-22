70bbfadebd678bf948f9f1f4b6f85bc343ff9bce
yeti-detective <yeti.detective@gmail.com>
2018-05-22 18:11:29 -0400

    continue work on how_far_can_you_go method

--- slideable.rb
+++ slideable.rb
@@ -1,2 +1,34 @@
 module Slideable
+  CROSS_DIRECTIONS = [
+    [-1, 0],
+    [0, 1],
+    [-1, 0],
+    [0, -1]
+  ].freeze
+
+  X_DIRECTIONS = [
+    [-1, -1],
+    [-1, 1],
+    [1, 1],
+    [1, -1]
+  ]
+
+  def horizontal_dirs
+    CROSS_DIRECTIONS
+  end
+
+  def diagonal_dirs
+    X_DIRECTIONS
+  end
+
+  def moves
+    moves = []
+    move_dirs.each do |dx, dy|
+
+    end
+  end
+
+  def how_far_can_you_go(dx, dy)
+    x, y = pos
+  end
 end
