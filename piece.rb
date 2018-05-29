c06091adecd61e740f95420584a02ffc2da77dfc
yeti-detective <yeti.detective@gmail.com>
2018-05-29 18:07:48 -0400

    gave method for changing color so highlight can work

--- piece.rb
+++ piece.rb
@@ -7,17 +7,25 @@ class Piece
     @pos = pos
   end
 
+  def color=(color)
+    @color = color
+  end
+
   def moves
     moves = []
     if move_dirs.include?('diagonal')
       up_right = [@pos[0] + 1, @pos[1] + 1]
     end
+    moves
   end
 
   def update_pos(pos)
     @pos = pos
   end
 
+  def select
+    @board.highlight_moves(self.moves)
+  end
   # private
   attr_reader :pos, :board
   # def to_s
