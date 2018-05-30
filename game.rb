20acfff0b91d81d6509364fe30b3f70d0d9c4f20
yeti-detective <yeti.detective@gmail.com>
2018-05-30 14:17:50 -0400

    when cursor selects piece, dup board is generated & displays possible moves

--- game.rb
+++ game.rb
@@ -1,14 +1,35 @@
 class Game
-  attr_reader :display
+  attr_reader :display, :cursor, :board
 
   def initialize(display)
     @display = display
+    @board = display.board
+    @cursor = display.cursor
   end
 
   def play
     until game_over
       display.render
-      display.cursor.get_input
+      pos = cursor.get_input
+      if !board[pos].is_a?(NullPiece)
+        selected_piece(board[pos])
+      end
+    end
+
+  end
+
+  def selected_piece(piece)
+    fake_board = board.dup
+    fake_cursor = cursor.class.new(cursor.cursor_pos, fake_board)
+    fake_display = display.class.new(cursor, fake_board)
+    fake_board[piece.pos].select
+    fake_display.render
+    moved = false
+    selection = {}
+    until moved
+      next_moves = piece.moves.concat([piece.pos])
+      selection = fake_cursor.cycle_moves(next_moves)
+      moved = selection[:finished]
     end
 
   end
